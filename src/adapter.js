4f7ea32b4f1a1a190fe773f8c8102b131fce9b13
Sakthi10122004 <sakthikaribeeran@gmail.com>
2026-06-04 18:02:25 +0530

    chore: bump version to 1.1.2 and disable caching on inject endpoint

--- src/adapter.js
+++ src/adapter.js
@@ -73,6 +73,9 @@ class MailconfigAdapter extends SchedulingDefault {
             mailconfigApp.use(express.json());
             
             mailconfigApp.get('/ghost/mailconfig/frontend-inject.js', (req, res) => {
+                res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+                res.setHeader('Pragma', 'no-cache');
+                res.setHeader('Expires', '0');
                 res.sendFile(path.join(__dirname, 'frontend-inject.js'));
             });
             
